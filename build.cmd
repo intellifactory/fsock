e62a3b6b91aceecc8ec8ea342e6c1bb924602dba
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2014-04-16 09:48:02 -0400

    Fix build.cmd

--- build.cmd
+++ build.cmd
@@ -3,7 +3,6 @@ setlocal
 set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0
 set PATH=%PATH%;%ProgramFiles%\Microsoft SDKs\F#\3.1\Framework\v4.0
 set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-tools\NuGet.exe install SharpCompress -version 0.10.3 -o packages -excludeVersion
-MSBuild.exe src/FsNuGet/FsNuGet.fsproj /p:Configuration=Release /p:VisualStudioVersion=12.0
+MSBuild.exe src/FSock/FSock.fsproj /p:Configuration=Release /p:VisualStudioVersion=12.0
 if not exist build mkdir build
-tools\NuGet.exe pack FsNuGet.nuspec -outputDirectory build -Version %APPVEYOR_BUILD_VERSION%
+tools\NuGet.exe pack FSock.nuspec -outputDirectory build -Version %APPVEYOR_BUILD_VERSION%
